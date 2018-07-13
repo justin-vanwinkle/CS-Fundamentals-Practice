40869750b1f06cd36289d1894327d1a0e5b4ce5c
Justin VanWinkle <vanwinkle.justin@gmail.com>
2018-07-12 22:27:13 -0400

    bubble sort optimization

--- Sorting/BubbleSorter.cs
+++ Sorting/BubbleSorter.cs
@@ -10,12 +10,18 @@ namespace Sorting
             do
             {
                 swapFound = false;
-                for (var i = 0; i < array.Length - 1; i++)
+                
+                // used to track the earliest point in the array that is fully sorted
+                var firstSortedElement = array.Length;
+
+                for (var i = 0; i < firstSortedElement - 1; i++)
                 {
                     var comparison = array[i].CompareTo(array[i + 1]);
                     if (comparison > 0)
                     {
                         swapFound = true;
+                        firstSortedElement = i + 1;
+                        
                         var temp = array[i + 1];
                         array[i + 1] = array[i];
                         array[i] = temp;                
--- Tests/SortingTests.cs
+++ Tests/SortingTests.cs
@@ -8,14 +8,16 @@ namespace Tests
 {
     public class SortingTests
     {
-        BubbleSorter<int> _bubbleSorter = new BubbleSorter<int>();
 
         [Fact]
         public void Can_Bubble_Sort()
         {
-            var result = _bubbleSorter.Sort(new[] {5, 1, 4, 2, 8});
-            var x = 1;
+            var bubbleSorter = new BubbleSorter<int>();
+            var result = bubbleSorter.Sort(new[] {9, 5, 1, 4, 2, 8, 2});
             result.Should().BeOfType<int[]>();
+            result.Should().BeInAscendingOrder();
+
         }
+        
     }
 }
